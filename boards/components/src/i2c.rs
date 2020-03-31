77a63c274c8235926e536b5da56c5c94fce90c52
Alexandru Radovici <msg4alex@gmail.com>
2020-03-31 19:56:01 +0300

    set client i2c

--- boards/components/src/i2c.rs
+++ boards/components/src/i2c.rs
@@ -40,7 +40,7 @@ use kernel::static_init;
 // }
 
 pub struct I2CMuxComponent {
-	i2c: &'static dyn i2c::I2CMaster,
+    i2c: &'static dyn i2c::I2CMaster,
 }
 
 // pub struct I2CSyscallComponent {
@@ -49,27 +49,27 @@ pub struct I2CMuxComponent {
 // }
 
 pub struct I2CComponent {
-	i2c_mux: &'static MuxI2C<'static>,
-	address: u8,
+    i2c_mux: &'static MuxI2C<'static>,
+    address: u8,
 }
 
 impl I2CMuxComponent {
-	pub fn new(i2c: &'static dyn i2c::I2CMaster) -> Self {
-		I2CMuxComponent { i2c: i2c }
-	}
+    pub fn new(i2c: &'static dyn i2c::I2CMaster) -> Self {
+        I2CMuxComponent { i2c: i2c }
+    }
 }
 
 impl Component for I2CMuxComponent {
-	type StaticInput = ();
-	type Output = &'static MuxI2C<'static>;
+    type StaticInput = ();
+    type Output = &'static MuxI2C<'static>;
 
-	unsafe fn finalize(self, _static_buffer: Self::StaticInput) -> Self::Output {
-		let mux_i2c = static_init!(MuxI2C<'static>, MuxI2C::new(self.i2c));
+    unsafe fn finalize(self, _static_buffer: Self::StaticInput) -> Self::Output {
+        let mux_i2c = static_init!(MuxI2C<'static>, MuxI2C::new(self.i2c));
 
-		self.i2c.set_client(mux_i2c);
+        self.i2c.set_client(mux_i2c);
 
-		mux_i2c
-	}
+        mux_i2c
+    }
 }
 
 // impl I2CSyscallComponent {
@@ -107,24 +107,24 @@ impl Component for I2CMuxComponent {
 // }
 
 impl I2CComponent {
-	pub fn new(mux: &'static MuxI2C<'static>, address: u8) -> Self {
-		I2CComponent {
-			i2c_mux: mux,
-			address: address,
-		}
-	}
+    pub fn new(mux: &'static MuxI2C<'static>, address: u8) -> Self {
+        I2CComponent {
+            i2c_mux: mux,
+            address: address,
+        }
+    }
 }
 
 impl Component for I2CComponent {
-	type StaticInput = ();
-	type Output = &'static I2CDevice<'static>;
+    type StaticInput = ();
+    type Output = &'static I2CDevice<'static>;
 
-	unsafe fn finalize(self, static_buffer: Self::StaticInput) -> Self::Output {
-		let i2c_device = static_init!(
-			I2CDevice<'static>,
-			I2CDevice::new(self.i2c_mux, self.address)
-		);
+    unsafe fn finalize(self, static_buffer: Self::StaticInput) -> Self::Output {
+        let i2c_device = static_init!(
+            I2CDevice<'static>,
+            I2CDevice::new(self.i2c_mux, self.address)
+        );
 
-		i2c_device
-	}
+        i2c_device
+    }
 }
